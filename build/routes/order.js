c1b63001dea6bd3314360dfa017f43245016a249
Violetbee <caglarkarahuseyin28@gmail.com>
2025-07-07 13:21:32 +0300

    .

--- build/routes/order.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const order_1 = require("../controllers/order");
-const router = (0, express_1.Router)();
-router.get("/", order_1.getAllOrders);
-router.post("/create", order_1.createOrder);
-router.delete("/:id", order_1.deleteOrder);
-exports.default = router;
